8864490f6e70febc843b121a6aa3ce75d271ce7c
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-19 11:27:33 -0400

    did excersice

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"simulating a deposit of {amount, 0:c} to your account!");
+            _balance = amount;
+
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+
+            Console.WriteLine("How much $$$ do you want to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            ba.Deposit(amountToDeposit);
+            double userBalance = ba.GetBalance();
+
+            Console.WriteLine($"Your current balance is {userBalance,0:c}");
+            
+        }
+    }
+}
